a5d4b419a461ad2f43701f232f329c611d0672e6
kl3jvi <klejvisiper@gmail.com>
2022-01-27 21:33:13 +0100

    adding viewpager for intro

---
+++ app/src/main/java/com/toggle/ui/activities/AppIntroActivity.kt
@@ -0,0 +1,53 @@
+package com.toggle.ui.activities
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.github.appintro.AppIntro
+import com.github.appintro.AppIntroFragment
+import com.toggle.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class AppIntroActivity : AppIntro() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        applicationContext
+
+        addSlide(
+            AppIntroFragment.createInstance(
+                title = "The title of your slide",
+                description = "A description that will be shown on the bottom",
+                imageDrawable = R.drawable.ic_first_slide,
+                titleColorRes = R.color.primaryTextColor,
+                descriptionColorRes = R.color.secondaryTextColor,
+                backgroundColorRes = R.color.white,
+            )
+        )
+
+        addSlide(
+            AppIntroFragment.createInstance(
+                title = "The title of your slide",
+                description = "A description that will be shown on the bottom",
+                titleColorRes = R.color.primaryTextColor,
+                descriptionColorRes = R.color.secondaryTextColor,
+                backgroundColorRes = R.color.white,
+            )
+        )
+
+    }
+
+    override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+        // Decide what to do when the user clicks on "Skip"
+        finish()
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        // Decide what to do when the user clicks on "Done"
+        finish()
+    }
+}
+
